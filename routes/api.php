ec98160068ce2dc3faaecd9453d6e3e8ffab1ff5
EricJohnMartinez <ericjohn121709@gmail.com>
2023-03-17 10:46:40 +0800

    routes auth

--- routes/api.php
+++ routes/api.php
@@ -25,15 +25,16 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post('/login', [UserController::class,'login']);
 Route::post('/register', [UserController::class,'register']);
 
-Route::get('/cars', [CarsController::class, 'index']);
-Route::get('/cars/{id}', [CarsController::class, 'show']);
-Route::post('/cars', [CarsController::class, 'store']);
-Route::put('/cars/{id}', [CarsController::class, 'update']);
-Route::delete('/cars/{id}', [CarsController::class, 'destroy']);
+
 
 
 Route::group(['middleware' => ['auth:sanctum']],function(){
     
+    Route::get('/cars', [CarsController::class, 'index']);
+    Route::get('/cars/{id}', [CarsController::class, 'show']);
+    Route::post('/cars', [CarsController::class, 'store']);
+    Route::put('/cars/{id}', [CarsController::class, 'update']);
+    Route::delete('/cars/{id}', [CarsController::class, 'destroy']);
 
 
     Route::post('/reservations', [ReservationController::class, 'store']);
