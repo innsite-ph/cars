6c075ef0e65f202500b9b97396935d9c861188ab
Lee-mar-Arcon <arconleemar.innsite@gmail.com>
2023-03-23 14:43:58 +0800

    reserve car api function modified

--- app/Http/Controllers/CarsController.php
+++ app/Http/Controllers/CarsController.php
@@ -6,6 +6,7 @@ use App\Models\Cars;
 use App\Models\Reservation;
 use Illuminate\Http\Request;
 use App\Http\Requests\CarRequest;
+use Illuminate\Support\Facades\DB;
 
 class CarsController extends Controller
 {
@@ -116,101 +117,80 @@ class CarsController extends Controller
 
     public function search_car(Request $request)
     {
-        $car = $request->input("car");
-        $checkInDate = strtotime($request->input('checkInDate'));
-        $checkOutDate = strtotime($request->input('checkOutDate'));
-        $ids = [];
-        $cars = Cars::select('brand', 'id', 'colour', 'model', 'power', 'fuel_type', 'license_plate', 'car_year')->where('brand', 'like', '%' . $car . '%')
-            ->orWhere('colour', 'like', '%' . $car . '%')
-            ->orWhere('model', 'like', '%' . $car . '%')
-            ->orWhere('vin', 'like', '%' . $car . '%')
-            ->orWhere('license_plate', 'like', '%' . $car . '%')
-            ->orWhere('gearbox_type', 'like', '%' . $car . '%')
-            ->orWhere('fuel_type', 'like', '%' . $car . '%')
-            ->orWhere('engine_capacity', 'like', '%' . $car . '%')
-            ->orWhere('power', 'like', '%' . $car . '%')
-            ->orWhere('car_year', 'like', '%' . $car . '%')->get();
-        $reservations = Reservation::select('check_in_date', 'check_out_date', 'car_id')->get();
-        for ($c = 0; $c < count($cars); $c++) {
-            $not_available = true;
-            for ($r = 0; $r < count($reservations); $r++) {
-                // CHECK IF RESERVATION IS FOR THE CAR
-                if ($reservations[$r]['car_id'] == $cars[$c]['id']) {
-                    // CHECK IF CAR IS ALREADY INSIDE IDS
-                    if (!in_array($cars[$c]['id'], $ids)) {
-                        if ($checkInDate > strtotime($reservations[$r]['check_in_date']) && $checkInDate < strtotime($reservations[$r]['check_out_date'])) {
-                            array_push($ids, array('car_id' => $cars[$c]['id'], 'not_available' => true, 'car_year' => $cars[$c]['car_year'], 'license_plate' => $cars[$c]['license_plate'], 'fuel_type' => $cars[$c]['fuel_type'], 'power' => $cars[$c]['power'], 'model' => $cars[$c]['model'], 'colour' => $cars[$c]['colour'], 'brand' => $cars[$c]['brand']));
-                            $not_available = false;
-                            break;
-                        } else if ($checkOutDate > strtotime($reservations[$r]['check_in_date']) && $checkOutDate < strtotime($reservations[$r]['check_out_date'])) {
-                            array_push($ids, array('car_id' => $cars[$c]['id'], 'not_available' => true, 'car_year' => $cars[$c]['car_year'], 'license_plate' => $cars[$c]['license_plate'], 'fuel_type' => $cars[$c]['fuel_type'], 'power' => $cars[$c]['power'], 'model' => $cars[$c]['model'], 'colour' => $cars[$c]['colour'], 'brand' => $cars[$c]['brand']));
-                            $not_available = false;
-                            break;
-                        } else if ($checkOutDate > strtotime($reservations[$r]['check_out_date']) && $checkInDate < strtotime($reservations[$r]['check_in_date'])) {
-                            // condition kapag yung check in at check out ay nasa loob ng input na check in at check out. dapat not available din kapag ganon
-                            array_push($ids, array('car_id' => $cars[$c]['id'], 'not_available' => true, 'car_year' => $cars[$c]['car_year'], 'license_plate' => $cars[$c]['license_plate'], 'fuel_type' => $cars[$c]['fuel_type'], 'power' => $cars[$c]['power'], 'model' => $cars[$c]['model'], 'colour' => $cars[$c]['colour'], 'brand' => $cars[$c]['brand']));
-                            $not_available = false;
-                            break;
-                        }
-                    }
-                } else continue;
-            }
-            if ($not_available) {
-                array_push($ids, array('car_id' => $cars[$c]['id'], 'not_available' => false, 'car_year' => $cars[$c]['car_year'], 'license_plate' => $cars[$c]['license_plate'], 'fuel_type' => $cars[$c]['fuel_type'], 'power' => $cars[$c]['power'], 'model' => $cars[$c]['model'], 'colour' => $cars[$c]['colour'], 'brand' => $cars[$c]['brand']));
-            }
-        }
-        return $ids;
+        $car = "%" . $request->input("car") . "%";
+        $checkInDate = '2020-01-01 01:00:00';
+        $checkOutDate = '2020-01-01 01:00:00';
+        // $ids = [];
+        // $cars = Cars::select('brand', 'id', 'colour', 'model', 'power', 'fuel_type', 'license_plate', 'car_year')->where('brand', 'like', '%' . $car . '%')
+        //     ->orWhere('colour', 'like', '%' . $car . '%')
+        //     ->orWhere('model', 'like', '%' . $car . '%')
+        //     ->orWhere('vin', 'like', '%' . $car . '%')
+        //     ->orWhere('license_plate', 'like', '%' . $car . '%')
+        //     ->orWhere('gearbox_type', 'like', '%' . $car . '%')
+        //     ->orWhere('fuel_type', 'like', '%' . $car . '%')
+        //     ->orWhere('engine_capacity', 'like', '%' . $car . '%')
+        //     ->orWhere('power', 'like', '%' . $car . '%')
+        //     ->orWhere('car_year', 'like', '%' . $car . '%')->get();
+        // $reservations = Reservation::select('check_in_date', 'check_out_date', 'car_id')->get();
+        // for ($c = 0; $c < count($cars); $c++) {
+        //     $not_available = true;
+        //     for ($r = 0; $r < count($reservations); $r++) {
+        //         // CHECK IF RESERVATION IS FOR THE CAR
+        //         if ($reservations[$r]['car_id'] == $cars[$c]['id']) {
+        //             // CHECK IF CAR IS ALREADY INSIDE IDS
+        //             if (!in_array($cars[$c]['id'], $ids)) {
+        //                 if ($checkInDate > strtotime($reservations[$r]['check_in_date']) && $checkInDate < strtotime($reservations[$r]['check_out_date'])) {
+        //                     array_push($ids, array('car_id' => $cars[$c]['id'], 'not_available' => true, 'car_year' => $cars[$c]['car_year'], 'license_plate' => $cars[$c]['license_plate'], 'fuel_type' => $cars[$c]['fuel_type'], 'power' => $cars[$c]['power'], 'model' => $cars[$c]['model'], 'colour' => $cars[$c]['colour'], 'brand' => $cars[$c]['brand']));
+        //                     $not_available = false;
+        //                     break;
+        //                 } else if ($checkOutDate > strtotime($reservations[$r]['check_in_date']) && $checkOutDate < strtotime($reservations[$r]['check_out_date'])) {
+        //                     array_push($ids, array('car_id' => $cars[$c]['id'], 'not_available' => true, 'car_year' => $cars[$c]['car_year'], 'license_plate' => $cars[$c]['license_plate'], 'fuel_type' => $cars[$c]['fuel_type'], 'power' => $cars[$c]['power'], 'model' => $cars[$c]['model'], 'colour' => $cars[$c]['colour'], 'brand' => $cars[$c]['brand']));
+        //                     $not_available = false;
+        //                     break;
+        //                 } else if ($checkOutDate > strtotime($reservations[$r]['check_out_date']) && $checkInDate < strtotime($reservations[$r]['check_in_date'])) {
+        //                     // condition kapag yung check in at check out ay nasa loob ng input na check in at check out. dapat not available din kapag ganon
+        //                     array_push($ids, array('car_id' => $cars[$c]['id'], 'not_available' => true, 'car_year' => $cars[$c]['car_year'], 'license_plate' => $cars[$c]['license_plate'], 'fuel_type' => $cars[$c]['fuel_type'], 'power' => $cars[$c]['power'], 'model' => $cars[$c]['model'], 'colour' => $cars[$c]['colour'], 'brand' => $cars[$c]['brand']));
+        //                     $not_available = false;
+        //                     break;
+        //                 }
+        //             }
+        //         } else continue;
+        //     }
+        //     if ($not_available) {
+        //         array_push($ids, array('car_id' => $cars[$c]['id'], 'not_available' => false, 'car_year' => $cars[$c]['car_year'], 'license_plate' => $cars[$c]['license_plate'], 'fuel_type' => $cars[$c]['fuel_type'], 'power' => $cars[$c]['power'], 'model' => $cars[$c]['model'], 'colour' => $cars[$c]['colour'], 'brand' => $cars[$c]['brand']));
+        //     }
+        // }
+        // return $ids;
+        // $cars =  collect(DB::select("SELECT c.model, (SELECT r.car_id FROM reservations AS r WHERE r.car_id = c.id AND ((? BETWEEN r.check_in_date AND r.check_out_date OR ? BETWEEN r.check_in_date AND r.check_out_date) OR (r.check_in_date BETWEEN ? AND ? AND r.check_out_date BETWEEN ? AND ?))) AS not_available
+        // FROM cars AS c where c.model like ?", [$checkInDate, $checkOutDate, $checkInDate, $checkOutDate, $checkInDate, $checkOutDate, $car]));
+        // $perPage = 10;
+        // $page = 1;
+
+        // $cars = $cars->paginate($perPage, ['*'], 'page', $page);
+        $cars = collect(DB::select("SELECT c.model, (SELECT r.car_id FROM reservations AS r WHERE r.car_id = c.id AND ((? BETWEEN r.check_in_date AND r.check_out_date OR ? BETWEEN r.check_in_date AND r.check_out_date) OR (r.check_in_date BETWEEN ? AND ? AND r.check_out_date BETWEEN ? AND ?))) AS not_available,    c.id,
+        c.car_year,
+        c.license_plate,
+        c.fuel_type,
+        c.power,
+        c.model,
+        c.colour,
+        c.brand
+    FROM cars AS c where c.model like ?", [$checkInDate, $checkOutDate, $checkInDate, $checkOutDate, $checkInDate, $checkOutDate, $car]));
+
+        $perPage = 10;
+        $page = 1;
+
+        $cars = new \Illuminate\Pagination\LengthAwarePaginator(
+            $cars->forPage($page, $perPage),
+            $cars->count(),
+            $perPage,
+            $page,
+            [
+                'path' => \Illuminate\Pagination\Paginator::resolveCurrentPath(),
+                'pageName' => 'page',
+            ]
+        );
+
+        return $cars;
     }
-
-
-    // public function search_car(Request $request)
-    // {
-    //     $car = $request->input("car");
-    //     $checkInDate = strtotime($request->input('checkInDate'));
-    //     $checkOutDate = strtotime($request->input('checkOutDate'));
-    //     $ids = [];
-    //     $cars = Cars::select('brand', 'id', 'colour', 'model', 'power', 'fuel_type', 'license_plate', 'car_year')->where('brand', 'like', '%' . $car . '%')
-    //         ->orWhere('colour', 'like', '%' . $car . '%')
-    //         ->orWhere('model', 'like', '%' . $car . '%')
-    //         ->orWhere('vin', 'like', '%' . $car . '%')
-    //         ->orWhere('license_plate', 'like', '%' . $car . '%')
-    //         ->orWhere('gearbox_type', 'like', '%' . $car . '%')
-    //         ->orWhere('fuel_type', 'like', '%' . $car . '%')
-    //         ->orWhere('engine_capacity', 'like', '%' . $car . '%')
-    //         ->orWhere('power', 'like', '%' . $car . '%')
-    //         ->orWhere('car_year', 'like', '%' . $car . '%')->paginate();
-
-    //     $reservations = Reservation::select('check_in_date', 'check_out_date', 'car_id')->get();
-
-    //     for ($c = 0; $c < count($cars); $c++) {
-    //         $not_available = true;
-    //         for ($r = 0; $r < count($reservations); $r++) {
-    //             // CHECK IF RESERVATION IS FOR THE CAR
-    //             if ($reservations[$r]['car_id'] == $cars[$c]['id']) {
-    //                 // CHECK IF CAR IS ALREADY INSIDE IDS
-    //                 if (!in_array($cars[$c]['id'], $ids)) {
-    //                     if ($checkInDate > strtotime($reservations[$r]['check_in_date']) && $checkInDate < strtotime($reservations[$r]['check_out_date'])) {
-    //                         array_push($ids, array('car_id' => $cars[$c]['id'], 'not_available' => true, 'car_year' => $cars[$c]['car_year'], 'license_plate' => $cars[$c]['license_plate'], 'fuel_type' => $cars[$c]['fuel_type'], 'power' => $cars[$c]['power'], 'model' => $cars[$c]['model'], 'colour' => $cars[$c]['colour'], 'brand' => $cars[$c]['brand']));
-    //                         $not_available = false;
-    //                         break;
-    //                     } else if ($checkOutDate > strtotime($reservations[$r]['check_in_date']) && $checkOutDate < strtotime($reservations[$r]['check_out_date'])) {
-    //                         array_push($ids, array('car_id' => $cars[$c]['id'], 'not_available' => true, 'car_year' => $cars[$c]['car_year'], 'license_plate' => $cars[$c]['license_plate'], 'fuel_type' => $cars[$c]['fuel_type'], 'power' => $cars[$c]['power'], 'model' => $cars[$c]['model'], 'colour' => $cars[$c]['colour'], 'brand' => $cars[$c]['brand']));
-    //                         $not_available = false;
-    //                         break;
-    //                     } else if ($checkOutDate > strtotime($reservations[$r]['check_out_date']) && $checkInDate < strtotime($reservations[$r]['check_in_date'])) {
-    //                         // condition kapag yung check in at check out ay nasa loob ng input na check in at check out. dapat not available din kapag ganon
-    //                         array_push($ids, array('car_id' => $cars[$c]['id'], 'not_available' => true, 'car_year' => $cars[$c]['car_year'], 'license_plate' => $cars[$c]['license_plate'], 'fuel_type' => $cars[$c]['fuel_type'], 'power' => $cars[$c]['power'], 'model' => $cars[$c]['model'], 'colour' => $cars[$c]['colour'], 'brand' => $cars[$c]['brand']));
-    //                         $not_available = false;
-    //                         break;
-    //                     }
-    //                 }
-    //             } else continue;
-    //         }
-    //         if ($not_available) {
-    //             array_push($ids, array('car_id' => $cars[$c]['id'], 'not_available' => false, 'car_year' => $cars[$c]['car_year'], 'license_plate' => $cars[$c]['license_plate'], 'fuel_type' => $cars[$c]['fuel_type'], 'power' => $cars[$c]['power'], 'model' => $cars[$c]['model'], 'colour' => $cars[$c]['colour'], 'brand' => $cars[$c]['brand']));
-    //         }
-    //     }
-    //     $haha = $cars->getData(true);;
-    //     return var_dump($haha);
-    // }
 }
